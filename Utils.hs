bb0f86fe0c3eceb5e97ad10f8a4267af780a1cfe
Artur Gavkaliuk <mandeburka@gmail.com>
2015-08-20 14:51:06 +0300

    problem 9

--- Utils.hs
+++ Utils.hs
@@ -4,6 +4,12 @@ module Utils
 , isPrime
 , primeDividers
 , isPalindrome
+, primes
+, windows
+, lMax
+, lMin
+, pythagoreans
+, isPythagorean
 ) where
 
 fibo :: [Int]
@@ -27,3 +33,34 @@ primeDividers x = filter isPrime (takeWhile (< truncatedSqrt x) (dividers x))
 
 isPalindrome :: Int -> Bool
 isPalindrome x = (read . reverse . show $ x) == x
+
+
+primes :: [Int]
+primes = f [] where
+  f :: [Int] -> [Int]
+  f [] = 2 : f [2]
+  f (x:xs) =
+    let next = head . filter (\n -> all (\y -> n `mod` y /= 0) (x:xs)) $ [(x+1)..]
+    in  next : f (next : x : xs)
+
+windows :: Int -> [a] -> [[a]]
+windows n (xs)
+  | length xs <= n = [xs]
+  | otherwise = take n xs : (windows n . tail $ xs)
+
+lMax :: (Ord a, Bounded a) => [a] -> a
+lMax = foldr max minBound
+
+lMin :: (Ord a, Bounded a) => [a] -> a
+lMin = foldr min maxBound
+
+sqr:: Int -> Int
+sqr x = truncate ((fromIntegral x :: Float) ** 2)
+
+isPythagorean :: Int -> Int -> Int -> Bool
+isPythagorean a b c
+  | a < b && b < c = sqr a + sqr b == sqr c
+  | otherwise = False
+
+pythagoreans :: Int -> Int -> [[Int]]
+pythagoreans start stop = [[a, b, c]| a <- [start..stop], b <- [(a+1)..stop], c <- [(b+1)..stop], isPythagorean a b c]
